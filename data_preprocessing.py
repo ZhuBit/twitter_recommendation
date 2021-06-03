5c3aba1213f0a9426f88ef1c6517228fac354027
Dzan Operta <dzanoperta@yahoo.com>
2021-06-03 12:33:46 +0200

    initial preprocessing

---
+++ data_preprocessing.py
@@ -0,0 +1,84 @@
+import pandas as pd
+pd.set_option('display.max_columns', 24)
+pd.set_option('display.width', 1000)
+from sklearn.pipeline import Pipeline
+import feature_engineering as fe
+
+train_data_path = "data/one_hour.tsv"
+
+
+def read_train_data(train_data_path):
+    column_names = ["text_tokens", "hashtags", "tweet_id", "present_media", "present_links", "present_domains",
+                    "tweet_type", "language", "tweet_timestamp", "engaged_with_user_id",
+                    "engaged_with_user_follower_count", "engaged_with_user_following_count",
+                    "engaged_with_user_is_verified",
+                    "engaged_with_user_account_creation", "engaging_user_id", "engaging_user_follower_count",
+                    "engaging_user_following_count",
+                    "engaging_user_is_verified", "engaging_user_account_creation", "engaged_follows_engaging",
+                    "reply_timestamp",
+                    "retweet_timestamp", "retweet_with_comment_timestamp", "like_timestamp"]
+
+    return pd.read_csv(train_data_path, header=None, names=column_names, delimiter='\x01')
+
+
+def preprocess_data(data):
+    df = data.copy()
+
+    preprocessing_pipeline = Pipeline(
+        [
+            ('target_encoder', fe.TargetEncoder(columns=["reply_timestamp", "retweet_timestamp",
+                                                             "retweet_with_comment_timestamp", "like_timestamp"])),
+
+            # splitting list features and counting the number of elements in the lists
+            ('tab_list_spliter', fe.ListTabSplitter(columns=["hashtags", "present_media", "present_links",
+                                                             "present_domains"])),
+            ('count_elements', fe.ListCountEncoder(columns=["hashtags", "present_links", "present_domains",
+                                                            "present_media"])),
+
+            # one hot encode categorical columns
+            ('one_hot_encoder', fe.OneHotEncoder(columns=["tweet_type", "language", "engaged_with_user_is_verified",
+                                                          "engaging_user_is_verified", "engaged_follows_engaging"])),
+
+            # TODO 1. check what gives better results, quantiles or pure count values
+            # create categorical bins based on numeric quantiles
+            # ('numeric_bins', fe.NumericQuantileBucketOneHotEncoder(columns=["engaged_with_user_follower_count",
+            #                                                                 "engaged_with_user_following_count",
+            #                                                                 "engaging_user_follower_count",
+            #                                                                 "engaging_user_following_count"])),
+        ]
+    )
+
+    df = preprocessing_pipeline.fit_transform(df)
+
+    labels = df[["reply_timestamp", "retweet_timestamp", "retweet_with_comment_timestamp", "like_timestamp"]]
+    ids = df[["tweet_id", "engaging_user_id", "engaged_with_user_id"]]
+
+    df.drop(
+        columns=[
+            "reply_timestamp", "retweet_timestamp", "retweet_with_comment_timestamp", "like_timestamp",
+
+            "tweet_id", "engaging_user_id", "engaged_with_user_id",
+
+            # not using text tokens
+            "text_tokens",
+
+            # not using timestamps
+            "tweet_timestamp",
+
+            # drop preprocessed columns
+            "hashtags", "present_links", "present_domains", "present_media", "tweet_type", "language",
+            "engaged_with_user_is_verified", "engaging_user_is_verified", "engaged_follows_engaging"
+
+            # TODO 1.
+            # "engaged_with_user_follower_count", "engaged_with_user_following_count",
+            # "engaging_user_follower_count", "engaging_user_following_count"
+        ],
+        inplace=True)
+
+    return df, labels, ids
+
+
+if __name__ == '__main__':
+    df = read_train_data(train_data_path)
+    df, labels, ids = preprocess_data(df)
+    print(df.columns)
---
+++ feature_engineering.py
@@ -0,0 +1,97 @@
+from typing import List
+from pandas import DataFrame
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import KBinsDiscretizer
+
+
+class TargetEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, columns: List[str]):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+
+        is_na = None
+
+        for column in self.columns:
+            X.loc[~X[column].isna(), column] = 1
+            X.loc[X[column].isna(), column] = 0
+
+            if is_na is not None:
+                is_na = is_na & X[column] == 0
+            else:
+                is_na = X[column] == 0
+
+        return X
+
+
+class ListTabSplitter(BaseEstimator, TransformerMixin):
+    def __init__(self, columns: List[str]):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: DataFrame):
+        for column in self.columns:
+            X[column] = X[column].str.split('\t')
+
+        return X
+
+
+class OneHotEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        self.one_hot_encoder = preprocessing.OneHotEncoder(sparse=False, handle_unknown='ignore')
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        for column in self.columns:
+            result = self.one_hot_encoder.fit_transform(X[[column]])
+            columns = ['{}_{}'.format(column, i) for i in range(result.shape[1])]
+            tmp_df = pd.DataFrame(result, columns=columns)
+
+            X = pd.concat([X, tmp_df], axis=1, sort=False)
+        return X
+
+
+class ListCountEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, columns: List[str]):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        for column in self.columns:
+            X['{}_count'.format(column)] = X[column].map(lambda x: len(x) if not isinstance(x, float) else 0)
+
+        return X
+
+
+class NumericQuantileBucketOneHotEncoder:
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        est = KBinsDiscretizer(encode='onehot-dense', strategy='quantile')
+        for column in self.columns:
+            result = est.fit_transform(X[[column]])
+            columns = ['{}_{}'.format(column, i) for i in range(5)]
+
+            tmp_df = pd.DataFrame(result, columns=columns)
+
+            X = pd.concat([X, tmp_df], axis=1, sort=False)
+
+        return X
+
+
