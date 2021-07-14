921376de6006bb0d4b464b9db581185ddca6c630
Martin Suchon <martin.suchon@gmail.com>
2021-07-14 03:55:45 +0200

    UUCF working

--- UUCF_Pipeline.py
+++ UUCF_Pipeline.py
@@ -4,12 +4,19 @@ from data_preprocessing import split_data
 from Result import Result
 
 TRAIN_DATA_PATH = "data/train/one_hour"
-data_preprocessing = DataPreprocessing(TRAIN_DATA_PATH)
-X=data_preprocessing.read_train_data()
-X_train, X_test, y_train, y_test = split_data(X, X['like_timestamp'], test_size=0.2)
-UUCF=UUCF_classifier()
-UUCF.train(X_train,y_train,'like_timestamp')
-predictions=UUCF.predict_proba(X_test)
-result = Result('UUCF', UUCF)
-result.calculate_and_store_metrics(y_test, predictions)
-result.store_result()
+
+
+def main():
+    data_preprocessing = DataPreprocessing(TRAIN_DATA_PATH)
+    X = data_preprocessing.read_train_data()
+    X_train, X_test, y_train, y_test = split_data(X, X['like_timestamp'], test_size=0.2)
+    UUCF = UUCF_classifier()
+    UUCF.train(X_train, y_train, 'like_timestamp')
+    predictions = UUCF.predict_proba(X_test)
+    result = Result('UUCF', UUCF)
+    result.calculate_and_store_metrics(y_test, predictions)
+    result.store_result()
+
+
+if __name__ == "__main__":
+    main()
