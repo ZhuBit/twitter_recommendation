4fe57e804db71f8fb23edca7afe1ed79a6c1cd81
Martin Suchon <martin.suchon@gmail.com>
2021-07-16 14:50:06 +0200

    UUCF pipeline updated

--- UUCF_Pipeline.py
+++ UUCF_Pipeline.py
@@ -1,5 +1,6 @@
 from classifiers.UUCF_classifier import UUCF_classifier
 from data_preprocessing import DataPreprocessing
+import data_preprocessing
 from data_preprocessing import split_data
 from Result import Result
 
@@ -9,13 +10,12 @@ TYPE_OF_ENGAGEMENT='retweet_timestamp'
 
 
 def main():
-    data_preprocessing = DataPreprocessing(TRAIN_DATA_PATH)
-    X = data_preprocessing.read_train_data()
-    X=transform(X,TYPE_OF_ENGAGEMENT)
+    DP = DataPreprocessing(TRAIN_DATA_PATH)
+    X = DP.read_train_data()
+    X=data_preprocessing.transform_data_for_uucf(X,TYPE_OF_ENGAGEMENT)
     validation_data = DataPreprocessing(VALIDATION_DATA_PATH)
     Y=validation_data.read_train_data()
-    Y=transform(Y,TYPE_OF_ENGAGEMENT)
-    #X_train, X_test, y_train, y_test = split_data(X, X['like_timestamp'], test_size=0.2)
+    Y=data_preprocessing.transform_data_for_uucf(Y,TYPE_OF_ENGAGEMENT)
     print(len(Y))
     UUCF = UUCF_classifier()
     UUCF.train(X, X[TYPE_OF_ENGAGEMENT], TYPE_OF_ENGAGEMENT)
